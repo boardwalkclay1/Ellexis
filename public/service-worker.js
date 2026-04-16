6ba22ab5f42ddac980722d79cedc1d41e0e43d09
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-04-16 12:45:26 -0400

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,32 @@
+const CACHE = "ellexis-v1";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/css/styles.css",
+  "/js/app.js",
+  "/js/router.js",
+  "/js/state.js",
+  "/js/scraper.js",
+  "/manifest.json",
+  "/assets/icon-192.png",
+  "/assets/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
